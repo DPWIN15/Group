39db7e89741c0b41ae26c999102ce3b5f204277b
D1V15 <antondivis@gmail.com>
2015-03-03 10:20:16 -0800

    added buildSpells()
    All that is needed is the spells.txt and then create an ArrayList<Spell> spells and call buildSpells.

--- FileUtil.java
+++ FileUtil.java
@@ -17,4 +17,26 @@ public class FileUtil {
         fin.close();
         return myList;
     }
+    
+    //Opens Spells file and fills list with spells and returns the list JAD
+    public static ArrayList<Spell> buildSpells()throws FileNotFoundException
+    {
+        ArrayList<Spell> spells = new ArrayList<Spell>();
+        int j = 0;
+        int damage, coolDown;
+        String name, type;
+        File f = new File("spells.txt");
+        Scanner fin = new Scanner(f);
+
+        while(fin.hasNext())
+        {
+            name = fin.next();
+            damage = fin.nextInt();
+            coolDown = fin.nextInt();
+            type = fin.next();
+            fin.nextLine();
+            Spell temp = new Spell(name, damage, coolDown, type);
+            spells.add(temp);
+            j++;
+        }
 }
